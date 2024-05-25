bda8d029c9faa3aa5905ec4786b51f85fe5d7ae8
Stew Eucen <stew.eucen@gmail.com>
2024-05-25 11:21:27 +0900

    Add test for main-exports

---
+++ tests/__tests__/main-exports.js
@@ -0,0 +1,59 @@
+import core from '../../rules/core'
+import disableCoreStylistic from '../../rules/disable-core-stylistic'
+import stylisticJs from '../../rules/plugins/stylistic/js'
+import stylisticPlus from '../../rules/plugins/stylistic/plus'
+import jestPlugin from '../../rules/plugins/jest'
+import jsdocPlugin from '../../rules/plugins/jsdoc'
+
+import mainExports from '../../index'
+
+describe('eslintConfig', () => {
+  describe('to have property', () => {
+    const cases = [
+      {
+        args: {
+          propertyName: 'core',
+          ruleSet: core,
+        },
+      },
+      {
+        args: {
+          propertyName: 'disableCoreStylistic',
+          ruleSet: disableCoreStylistic,
+        },
+      },
+      {
+        args: {
+          propertyName: 'stylisticJs',
+          ruleSet: stylisticJs,
+        },
+      },
+      {
+        args: {
+          propertyName: 'stylisticPlus',
+          ruleSet: stylisticPlus,
+        },
+      },
+      {
+        args: {
+          propertyName: 'jest',
+          ruleSet: jestPlugin,
+        },
+      },
+      {
+        args: {
+          propertyName: 'jsdoc',
+          ruleSet: jsdocPlugin,
+        },
+      },
+    ]
+
+    test.each(cases)('property: $args.propertyName', ({ args }) => {
+      expect(mainExports)
+        .toHaveProperty(
+          args.propertyName,
+          args.ruleSet
+        )
+    })
+  })
+})
