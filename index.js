14ada1fea761fc819f02d51f606990129e634f0a
Stew Eucen <stew.eucen@gmail.com>
2024-05-28 09:57:41 +0900

    Purge Jest plugin from main exports

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const core = require('./rules/core.js')
 const disableCoreStylistic = require('./rules/disable-core-stylistic.js')
 const stylisticJs = require('./rules/plugins/stylistic/js.js')
 const stylisticPlus = require('./rules/plugins/stylistic/plus.js')
-const jestPlugin = require('./rules/plugins/jest.js')
 const jsdocPlugin = require('./rules/plugins/jsdoc.js')
 
 module.exports = {
@@ -12,6 +11,5 @@ module.exports = {
   disableCoreStylistic,
   stylisticJs,
   stylisticPlus,
-  jest: jestPlugin,
   jsdoc: jsdocPlugin,
 }
