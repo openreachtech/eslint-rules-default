4cc3d6c61c7676aed81a0a501421435b97dc731b
Stew Eucen <stew.eucen@gmail.com>
2024-06-01 18:20:58 +0900

    Update require path in main-export files

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const core = require('./rules/core.js')
-const disableCoreStylistic = require('./rules/disable-core-stylistic.js')
-const stylistic = require('./rules/stylistic/all.js')
-const stylisticJs = require('./rules/stylistic/js.js')
-const stylisticPlus = require('./rules/stylistic/plus.js')
+const core = require('./configurations/core.js')
+const disableCoreStylistic = require('./configurations/disable-core-stylistic.js')
+const stylistic = require('./configurations/stylistic/all.js')
+const stylisticJs = require('./configurations/stylistic/js.js')
+const stylisticPlus = require('./configurations/stylistic/plus.js')
 
 module.exports = {
   core,
--- index.mjs
+++ index.mjs
@@ -1,11 +1,11 @@
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 
-export const core = require('./rules/core')
-export const disableCoreStylistic = require('./rules/disable-core-stylistic')
-export const stylistic = require('./rules/stylistic/all')
-export const stylisticJs = require('./rules/stylistic/js')
-export const stylisticPlus = require('./rules/stylistic/plus')
+export const core = require('./configurations/core')
+export const disableCoreStylistic = require('./configurations/disable-core-stylistic')
+export const stylistic = require('./configurations/stylistic/all')
+export const stylisticJs = require('./configurations/stylistic/js')
+export const stylisticPlus = require('./configurations/stylistic/plus')
 
 export default {
   core,
