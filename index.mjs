45e0e57ccdcfa6bc3602122dc316be6ec5ac4a5b
Stew Eucen <stew.eucen@gmail.com>
2024-06-01 16:35:41 +0900

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -3,12 +3,14 @@ const require = createRequire(import.meta.url)
 
 export const core = require('./rules/core')
 export const disableCoreStylistic = require('./rules/disable-core-stylistic')
+export const stylistic = require('./rules/stylistic/all')
 export const stylisticJs = require('./rules/stylistic/js')
 export const stylisticPlus = require('./rules/stylistic/plus')
 
 export default {
   core,
   disableCoreStylistic,
+  stylistic,
   stylisticJs,
   stylisticPlus,
 }
