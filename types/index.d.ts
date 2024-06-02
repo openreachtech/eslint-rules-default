caffb0c166e293c56a4976b879888ea8f1891d01
Stew Eucen <stew.eucen@gmail.com>
2024-06-03 03:24:05 +0900

    Add all-in-one stylistic type into types/index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,12 +5,14 @@ import {
 declare module '@openreachtech/eslint-rules-default' {
   const core: ESLint.ConfigData
   const disableCoreStylistic: ESLint.ConfigData
+  const stylistic: ESLint.ConfigData
   const stylisticJs: ESLint.ConfigData
   const stylisticPlus: ESLint.ConfigData
 
   export = {
     core,
     disableCoreStylistic,
+    stylistic,
     stylisticJs,
     stylisticPlus,
   }
