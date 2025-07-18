941df23ac043ccda9211e6e0d6ae94c3e4e728f0
Stew Eucen <stew.eucen@gmail.com>
2025-07-18 15:31:13 +0900

    Kick out stylistic/js.js from index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import core from './configurations/core.js'
 import stylistic from './configurations/stylistic.js'
-import stylisticJs from './configurations/stylistic/js.js'
 import stylisticPlus from './configurations/stylistic/plus.js'
 
 export default {
   core,
   stylistic,
-  stylisticJs,
   stylisticPlus,
 }
