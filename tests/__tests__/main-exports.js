45adb9ece64bd5608fd74a1426859b76347116da
Stew Eucen <stew.eucen@gmail.com>
2025-07-18 11:59:03 +0900

    Kick out disable-core-stylistic.js from test for main-exports

--- tests/__tests__/main-exports.js
+++ tests/__tests__/main-exports.js
@@ -1,5 +1,4 @@
 import core from '../../configurations/core.js'
-import disableCoreStylistic from '../../configurations/disable-core-stylistic.js'
 import stylistic from '../../configurations/stylistic/all.js'
 import stylisticJs from '../../configurations/stylistic/js.js'
 import stylisticPlus from '../../configurations/stylistic/plus.js'
@@ -13,12 +12,6 @@ describe('main exports', () => {
         args: {
           propertyName: 'core',
           ruleSet: core,
-        },
-      },
-      {
-        args: {
-          propertyName: 'disableCoreStylistic',
-          ruleSet: disableCoreStylistic,
         },
       },
       {
