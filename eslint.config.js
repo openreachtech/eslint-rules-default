fad54aeba5c1fa375706107ff4d97dddfcfb532f
Stew Eucen <stew.eucen@gmail.com>
2026-07-09 14:41:23 +0900

    Restore default value of allowTemplateLiterals option for @stylistic/quotes in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -78,7 +78,7 @@ export default [
         'single',
         {
           avoidEscape: true,
-          allowTemplateLiterals: 'never',
+          allowTemplateLiterals: false, // FIXME: Correct default value is 'never' by API reference, but it will throw on run `npx eslint .`
         },
       ],
       semi: [
